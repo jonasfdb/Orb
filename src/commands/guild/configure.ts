a741679002051adb83869aa26cb81465c6df10de
Jonas de Buhr <jonasdebuhr@gmail.com>
2025-09-02 18:03:37 +0200

    Replaced nanoid with ULID in captcha generator

---
+++ src/commands/guild/configure.ts
@@ -0,0 +1,11 @@
+import Discord from "discord.js";
+
+export default {
+  data: new Discord.SlashCommandBuilder()
+    .setName('configure')
+    .setDescription('description'),
+
+  async execute(client: Discord.Client<true>, interaction: Discord.ChatInputCommandInteraction) {
+    // what the command will do
+  }
+}
