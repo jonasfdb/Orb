982db8ad641fa696aaf1bd4d81ce795ab6c57c8f
Jonas de Buhr <jonasdebuhr@gmail.com>
2025-10-15 20:24:44 +0200

    Changed config command response methods and the way it interacts with the user

--- src/commands/guild/configure.ts
+++ src/commands/guild/configure.ts
@@ -1,5 +1,6 @@
 import Discord from "discord.js";
-import { validateCommandInteractionInGuild } from "../../util/validate";
+import { validateCommandInteractionInGuild, validateMessageInGuild } from "../../util/validate";
+import { emojis } from "../../../util/json/emojis";
 
 export default {
   data: new Discord.SlashCommandBuilder()
@@ -9,91 +10,204 @@ export default {
   async execute(client: Discord.Client<true>, interaction: Discord.ChatInputCommandInteraction) {
     validateCommandInteractionInGuild(interaction);
     await interaction.deferReply();
-
-    async function showConfigEmbed () {
-      const embConfig = new Discord.EmbedBuilder()
-        .setTitle('bing')
-        .setDescription('bong')
-        .addFields([
-          {
-            name: '**I.** Moderation',
-            value:  '\u{251C}[<:attention_icon_flat:1222305975822717120>] Swear Word List\n' +
-                    '\u{2514}[<:orb_toggle_b_enabled_flat:1222635342457339914>] Toxicity Analyzer',
-            inline: true,
-          },
-          {
-            name: '**II.** Welcoming',
-            value:  '\u{251C}[<:orb_toggle_b_enabled_flat:1222635342457339914>] Captcha Security\n' +
-                    '\u{251C}[<:orb_disabled:1222634792777023688>] Welcome/Leave channel\n' + 
-                    '\u{2514}[<:orb_disabled:1222634792777023688>] Welcome/Leave messages',
-            inline: true,
-          },
-          {
-            name: '**III.** Leveling',
-            value:  '\u{251C}[<:attention_icon_flat:1222305975822717120>] MEE6 Import\n' + 
-                    '\u{2514}[<:orb_toggle_b_enabled_flat:1222635342457339914>] Rewards',
-            inline: true,
-          },
-          {
-            name: '**IV.** Toggleables',
-            value: '(none)',
-            inline: true,
-          }
-        ])
-
-      const selSettings = new Discord.StringSelectMenuBuilder()
-        .setCustomId('selSettings')
-        .setPlaceholder('Select setting...')
-        .setOptions(
-          new Discord.StringSelectMenuOptionBuilder()
-            .setLabel('Moderation')
-            .setDescription('Modify how Orb protects you.')
-            .setValue('1'),
-          new Discord.StringSelectMenuOptionBuilder()
-            .setLabel('Welcoming and security')
-            .setDescription('Change how members get to join your server.')
-            .setValue('2'),
-          new Discord.StringSelectMenuOptionBuilder()
-            .setLabel('Leveling')
-            .setDescription('How Orb leveling works.')
-            .setValue('3'),
-          new Discord.StringSelectMenuOptionBuilder()
-            .setLabel('Toggleables')
-            .setDescription('Change some toggles for Orb.')
-            .setValue('4'),
-        )
-
-      const selSettingsRow = new Discord.ActionRowBuilder<Discord.StringSelectMenuBuilder>().addComponents(selSettings);
-
-      let reply = await interaction.editReply({ embeds: [embConfig], components: [selSettingsRow] });
-
-      try {
-        const collectorFilter = (selection: Discord.MessageComponentInteraction) => selection.user.id === interaction.user.id;
-        const confirmationCollector = reply.createMessageComponentCollector({ filter: collectorFilter, time: 30_000 });
-        
-        confirmationCollector.on('collect', (confirmation: Discord.StringSelectMenuInteraction) => {
-          switch (confirmation.values[0]) {
-            case '1':
-              interaction.editReply({ content: `${confirmation.customId}`, components: [] });
-              console.log("gee willickers")
-              break;
-            case '2':
-              interaction.editReply({ content: `${confirmation.customId}`, components: [] });
-              break;
-            case '3':
-              interaction.editReply({ content: `${confirmation.customId}`, components: [] });
-              break;
-            case '4':
-              interaction.editReply({ content: `${confirmation.customId}`, components: [] });
-              break;
-          }
-        });
-
-      } catch {
-        await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
-      }
-    }
-
-    showConfigEmbed();
+    // showConfigEmbed();
+    // showConfigMessage(interaction);
+    pathArray = [];
+    showSettingsMenu(interaction);
   }
+}
+
+let pathArray: any = [];
+
+const backButton = new Discord.ButtonBuilder()
+  .setCustomId('backButton')
+  .setLabel('Save and Return')
+  .setStyle(Discord.ButtonStyle.Primary)
+const saveButton = new Discord.ButtonBuilder()
+  .setCustomId('saveButton')
+  .setLabel('Save and Exit')
+  .setStyle(Discord.ButtonStyle.Success)
+const abortButton = new Discord.ButtonBuilder()
+  .setCustomId('abortButton')
+  .setLabel('Abort')
+  .setStyle(Discord.ButtonStyle.Danger)
+const navButtonRow = new Discord.ActionRowBuilder<Discord.ButtonBuilder>().addComponents(/*backButton,*/ saveButton, abortButton)
+
+async function navigateSettings(interaction: Discord.ChatInputCommandInteraction, message: Discord.Message<true>) {
+  try {
+    const collectorFilter = (selection: Discord.MessageComponentInteraction) => selection.user.id === interaction.user.id;
+    const navCollector = message.createMessageComponentCollector({ filter: collectorFilter, time: 30_000 });
+    
+    navCollector.on('collect', async (confirmation: Discord.ButtonInteraction) => {
+      console.log(confirmation)
+      await confirmation.deferUpdate();
+      switch (confirmation.customId) {
+        case 'backButton':
+          /*
+          // console.log(interaction);
+          pathArray[pathArray.length - 2](interactionGlobal); // execute function stored in array index-1
+          break;
+          */
+        case 'saveButton':
+          // TODO: Save routine here, and save error handler (perhaps sequelize.save()? could work yea)
+
+          const saveContainer = new Discord.ContainerBuilder()
+          .addTextDisplayComponents((textDisplay) =>
+            textDisplay.setContent(
+              `### ${emojis.success_emoji} - Changes saved!\n` +
+              'Successfully (and carefully) stored the changes you made.'
+            ),
+          )
+
+          await interaction.editReply({
+            components: [saveContainer],
+            flags: Discord.MessageFlags.IsComponentsV2,
+          });
+          break;
+        case 'abortButton':
+          const abortContainer = new Discord.ContainerBuilder()
+            .addTextDisplayComponents((textDisplay) =>
+              textDisplay.setContent(
+                `### ${emojis.failure_emoji} - Aborted\n` +
+                'Successfully aborted. No changes to your settings were made.'
+              ),
+            )
+
+          await interaction.editReply({
+            components: [abortContainer],
+            flags: Discord.MessageFlags.IsComponentsV2,
+          });
+          break;
+      }
+    });
+  } catch {
+    // TODO: Add timeout message as MessageComponentV2 too
+  }
+}
+
+async function showSettingsMenu(interaction: Discord.ChatInputCommandInteraction) {
+  // pathArray.push(showSettingsMenu);
+
+  const settingsContainer = new Discord.ContainerBuilder()
+    .addTextDisplayComponents((textDisplay) =>
+      textDisplay.setContent(
+        '## \u{1F9D9} - Settings Wizard\n' +
+        'Welcome to the Settings Wizard for this wonderful server. Please select a setting to change below.'
+      ),
+    )
+    .addSeparatorComponents((separator) => separator)
+    .addSectionComponents((section) => section
+      .addTextDisplayComponents(
+        (textDisplay) =>
+          textDisplay.setContent(
+            '### General\n' +
+            'Server locale, bot permissions, privacy, ...'
+          ),
+      )
+      .setButtonAccessory((button) =>
+        button.setCustomId('ebutton1').setLabel('Change...').setStyle(Discord.ButtonStyle.Primary),
+      )
+    )
+    .addSeparatorComponents((separator) => separator)
+    .addSectionComponents((section) => section
+      .addTextDisplayComponents(
+        (textDisplay) =>
+          textDisplay.setContent(
+            '### Welcoming\n' +
+            'Welcome/leave messages and channels, captcha, verification, ...'
+          ),
+      )
+      .setButtonAccessory((button) =>
+        button.setCustomId('ebutton2').setLabel('Change...').setStyle(Discord.ButtonStyle.Primary),
+      )
+    )
+    .addSeparatorComponents((separator) => separator)
+    .addSectionComponents((section) => section
+      .addTextDisplayComponents(
+        (textDisplay) =>
+          textDisplay.setContent(
+            '### Logging\n' +
+            'Log channels, what to log, ...'
+          ),
+      )
+      .setButtonAccessory((button) =>
+        button.setCustomId('ebutton3').setLabel('Change...').setStyle(Discord.ButtonStyle.Primary),
+      )
+    );
+
+  let reply = await interaction.editReply({
+    components: [settingsContainer],
+    flags: Discord.MessageFlags.IsComponentsV2,
+  });
+
+  try {
+    const collectorFilter = (selection: Discord.MessageComponentInteraction) => selection.user.id === interaction.user.id;
+    const confirmationCollector = reply.createMessageComponentCollector({ filter: collectorFilter, time: 30_000 });
+    
+    confirmationCollector.on('collect', (confirmation: Discord.StringSelectMenuInteraction) => {
+      console.log("collected")
+      console.log(confirmation)
+      switch (confirmation.customId) {
+        case 'ebutton1':
+          interaction.editReply({ content: `${confirmation.customId}`, components: [] });
+          break;
+        case 'ebutton2':
+          showConfigMessage(interaction);
+          break;
+        case 'ebutton3':
+          interaction.editReply({ content: `${confirmation.customId}`, components: [] });
+          break;
+        case 'ebutton4':
+          interaction.editReply({ content: `${confirmation.customId}`, components: [] });
+          break;
+      }
+    });
+
+  } catch {
+    // TODO: Add timeout message as MessageComponentV2 too
+  }
+}
+
+async function showConfigMessage(interaction: Discord.ChatInputCommandInteraction) {
+  // pathArray.push(showConfigMessage);
+
+  const exampleContainer = new Discord.ContainerBuilder()
+    .addTextDisplayComponents((textDisplay) =>
+      textDisplay.setContent(`Current welcome channel: <#${interaction.channel?.id}>`),
+    )
+    .addActionRowComponents((actionRow) =>
+      actionRow.setComponents(
+        new Discord.ChannelSelectMenuBuilder()
+          .setCustomId('welcomeChannelSelect')
+          .setPlaceholder('Change welcome channel...'),
+        ),
+    )
+    .addActionRowComponents((actionRow) =>
+      actionRow.setComponents(
+        new Discord.ChannelSelectMenuBuilder()
+          .setCustomId('leaveChannelSelect')
+          .setPlaceholder('Change leave channel...'),
+        ),
+    )
+    .addSeparatorComponents((separator) => separator)
+    .addTextDisplayComponents((textDisplay) =>
+      textDisplay.setContent(`Current leave message channel: <#${interaction.channel?.id}>`),
+    )
+    .addActionRowComponents((actionRow) =>
+      actionRow.setComponents(
+        new Discord.ChannelSelectMenuBuilder()
+          .setCustomId('exampleSelect2')
+          .setPlaceholder('Change...'), 
+        // new Discord.ButtonBuilder().setCustomId('exampleToggle').setLabel('Disable').setStyle(Discord.ButtonStyle.Danger),
+        // new Discord.ButtonBuilder().setCustomId('exampleToggle2').setLabel('Enable').setStyle(Discord.ButtonStyle.Success).setDisabled(),
+      ),
+    )
+
+  let reply = await interaction.editReply({
+    components: [exampleContainer, navButtonRow],
+    flags: Discord.MessageFlags.IsComponentsV2,
+  });
+  validateMessageInGuild(reply);
+  navigateSettings(interaction, reply);
+  return;
 }
